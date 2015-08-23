50ad1a29b302e764bf8e8813232aa805eeaa6fed
matthieu-eck <matthieu.eck@gmail.com>
2015-08-23 19:15:19 +0200

    Create new file

---
+++ rick.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: Utf-8 -*-
+rick=['give you up','let you down','run around and desert you','make you cry',
+      'say goodbye','tell a lie and hurt you']
+i=0
+for rickroll in rick:
+    print ("Never gonna "+(rickroll))
+    i=i+1
